2632d39e3f585f1e5e49441e5fa0b5cd3c9b1130
Suraj Kr. Mehta <surajmehta119@gmail.com>
2023-07-16 19:55:05 +0530

    Add files via upload

---
+++ ProjectVehicle/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+
+      SUV fortuner = new SUV("Fortuner",false);
+      fortuner.move(40,0);
+      fortuner.accelerate(20);
+      //fortuner.accelerate(-60);
+        System.out.println("Cureent Gear:"+fortuner.getCurrentGear());
+        System.out.println("Cureent Speed:"+fortuner.getCurrentSpeed());
+
+
+    }
+}
